b68b55df2ef25b4b0ba88ae2626eebaccdae5d2f
XXXandr84 <a.s.shmakov33@yandex.ru>
2023-11-04 11:58:03 +0000

    add files

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,7 +44,7 @@ connection {
   user = "ubuntu"
   agent = false
   # путь до приватного ключа
-  private_key = file("~/.ssh/id_ed25519")
+  private_key = file("/home/xandr/.ssh/id_ed25519")
   }
 
   provisioner "file" {
