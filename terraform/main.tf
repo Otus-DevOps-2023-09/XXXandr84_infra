2c3e76ffd647381fb33b61038f27fd6572e08d12
XXXandr84 <a.s.shmakov33@yandex.ru>
2023-11-05 16:35:22 +0000

    add files

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,13 +14,24 @@ provider "yandex" {
   zone                     = var.zone
 }
 
-resource "yandex_compute_instance" "app" {
-#  name = "reddit-base-1698741881"
-name = "reddit-app"
-  resources {
-    cores  = 2
-    memory = 2
-  }
+#resource "yandex_compute_instance" "app" {
+##  name = "reddit-base-1698741881"
+#name = "reddit-app"
+#  resources {
+#    cores  = 2
+#    memory = 2
+#  }
+
+resource "yandex_vpc_network" "app-network" {
+name = "reddit-app-network"
+}
+resource "yandex_vpc_subnet" "app-subnet" {
+name = "reddit-app-subnet"
+zone = "ru-central1-a"
+network_id = "${yandex_vpc_network.app-network.id}"
+v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
 
   boot_disk {
     initialize_params {
